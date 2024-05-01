7e7f7b9a3069926ba6cffb8c37d76227c1428e57
Victor on ROS2 VM <victor.dubois@free.fr>
2024-05-01 14:57:05 +0200

    split launch file between hardware and lidars

--- krabi_bringup/launch/krabi_hardware_launch.py
+++ krabi_bringup/launch/krabi_hardware_launch.py
@@ -10,9 +10,6 @@ from launch.substitutions import Command, LaunchConfiguration, PythonExpression
 from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
-    use_tim_instead_of_neato = LaunchConfiguration('use_tim_instead_of_neato', default='true')
-    use_lidar_loc = LaunchConfiguration('use_lidar_loc', default='False')
-
     #gpio_kraboss_node = Node(
     #    package='gpio_kraboss',
     #    executable='main.py',
@@ -35,42 +32,8 @@ def generate_launch_description():
         parameters=[{'port': '/dev/servos'}, {'baud': 57600}]
     )
 
-    neato_laser_publisher_node = Node(
-        package='xv_11_laser_driver',
-        executable='neato_laser_publisher',
-        name='neato_laser_publisher',
-        namespace="krabi_ns",
-        parameters=[{'port': '/dev/lidar'}, {'baud': 115200}, {'frame_id': 'krabby/neato_laser'}],
-        condition=UnlessCondition(use_tim_instead_of_neato)
-    )
-
-    tim_obstacles_group = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('krabi_bringup'),
-                'launch',
-                'tim_obstacles_launch.py'
-            ])
-        ])
-        ,condition=IfCondition(use_tim_instead_of_neato)
-    )
-
-    tim_loc_group = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('krabi_bringup'),
-                'launch',
-                'tim_loc_launch.py'
-            ])
-        ])
-        ,condition=IfCondition(use_lidar_loc)
-    )
-
     return LaunchDescription([
         #gpio_kraboss_node,
         motors_node,
-        servos_node,
-        neato_laser_publisher_node,
-        tim_obstacles_group,
-        tim_loc_group
+        servos_node
     ])
---
+++ krabi_bringup/launch/krabi_lidars_launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+import os
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch.conditions import IfCondition, UnlessCondition
+
+def generate_launch_description():
+    use_tim_instead_of_neato = LaunchConfiguration('use_tim_instead_of_neato', default='true')
+    use_lidar_loc = LaunchConfiguration('use_lidar_loc', default='False')
+
+    neato_laser_publisher_node = Node(
+        package='xv_11_laser_driver',
+        executable='neato_laser_publisher',
+        name='neato_laser_publisher',
+        namespace="krabi_ns",
+        parameters=[{'port': '/dev/lidar'}, {'baud': 115200}, {'frame_id': 'krabby/neato_laser'}],
+        condition=UnlessCondition(use_tim_instead_of_neato)
+    )
+
+    tim_obstacles_group = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('krabi_bringup'),
+                'launch',
+                'tim_obstacles_launch.py'
+            ])
+        ])
+        ,condition=IfCondition(use_tim_instead_of_neato)
+    )
+
+    tim_loc_group = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('krabi_bringup'),
+                'launch',
+                'tim_loc_launch.py'
+            ])
+        ])
+        ,condition=IfCondition(use_lidar_loc)
+    )
+
+    return LaunchDescription([
+        neato_laser_publisher_node,
+        tim_obstacles_group,
+        tim_loc_group
+    ])
