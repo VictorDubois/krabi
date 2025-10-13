4a994c3c9cf5b517559fc7975791e86a774c9233
Victor Dubois <victor.dubois@navya.tech>
2025-10-13 22:29:12 +0200

    2026 start position

--- krabi_bringup/launch/krabi_launch.py
+++ krabi_bringup/launch/krabi_launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
     )
     xRobotPos_launch_arg = DeclareLaunchArgument(
         'xRobotPos',
-        default_value='-0.75'
+        default_value='-1.25'
     )
     yRobotPos_launch_arg = DeclareLaunchArgument(
         'yRobotPos',
@@ -33,7 +33,7 @@ def generate_launch_description():
     )
     zRobotOrientation_launch_arg = DeclareLaunchArgument(
         'zRobotOrientation',
-        default_value='0.0'
+        default_value='1.570796327'
     )
     isSimulation_launch_arg = DeclareLaunchArgument(
         'isSimulation',
